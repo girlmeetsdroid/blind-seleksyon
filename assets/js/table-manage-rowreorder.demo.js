cdca8ecd6ebabab4828b618656035fb97709743a
Andrea Levinge <andrea@whitewidget.com>
2016-04-16 22:21:41 +0800

    Prelim support for draggable rows. Needs better touch areas

--- assets/js/table-manage-rowreorder.demo.js
+++ assets/js/table-manage-rowreorder.demo.js
@@ -1,13 +1,16 @@
 var handleDataTableRowReorder = function() {
 	"use strict";
-    
+
     if ($('#data-table').length !== 0) {
         $('#data-table').DataTable({
-            responsive: true,
             rowReorder: true,
             paging: false,
             searching:false,
-            select.info:false
+            info: false,
+            ordering:false,
+            "columnDefs": [
+            { "width": "10%", "targets": 0 }
+            ]
         });
     }
 };
