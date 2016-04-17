7e43f190cfe58b0e1e2cd1a5cf23d3ca6a03846e
arcilla <marvin.arcilla@nokia.com>
2016-04-17 12:38:19 +0800

    Not working

--- assets/js/table-manage-rowreorder.demo.js
+++ assets/js/table-manage-rowreorder.demo.js
@@ -2,19 +2,32 @@ var handleDataTableRowReorder = function() {
 	"use strict";
 
     if ($('.data-table').length !== 0) {
-        $('.data-table').DataTable({
-           rowReorder: {
-            selector: ':last-child',
-            snapX: 10
+      var table = $('.data-table').DataTable({
+         rowReorder: {
+          selector: ':last-child',
+          snapX: 10
 
-        },
-        paging: false,
-        searching:false,
-        ordering:false,
-        info: false,
-        scrollY: "400px",
-        scrollCollapse: true,
-    });
+	      },
+	      paging: false,
+	      searching:false,
+	      ordering:false,
+	      info: false,
+	      scrollY: "600px",
+	      scrollCollapse: true,
+    	});
+			table.on( 'row-reorder', function ( e, diff, edit ) {
+	        var result = 'Reorder started on row: '+edit.triggerRow.data()[1]+'<br>';
+
+	        for ( var i=0, ien=diff.length ; i<ien ; i++ ) {
+	            var rowData = table.row( diff[i].node ).data();
+
+	            result += rowData[1]+' updated to be in position '+
+	                diff[i].newData+' (was '+diff[i].oldData+')<br>';
+	        }
+
+	        console.log( 'Event result:<br>'+result );
+	    } );
+
     }
 };
 
